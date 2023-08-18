2fff5df9bf83d0981cf8a78ffb10e2dd374fc9e4
kuboi-isaac <123374858+kuboi-isaac@users.noreply.github.com>
2023-08-18 16:08:29 +0300

    admin changes

--- isaac/admin.js
+++ isaac/admin.js
@@ -1,15 +1,5 @@
-
-        function w3_open() {
-          document.getElementById("main").style.marginLeft = "25%";
-          document.getElementById("mySidebar").style.width = "25%";
-          document.getElementById("mySidebar").style.display = "block";
-          document.getElementById("openNav").style.display = 'inline-block';
-        }
-        function w3_close() {
-          document.getElementById("main").style.marginLeft = "0%";
-          document.getElementById("mySidebar").style.display = "none";
-          document.getElementById("openNav").style.display = "inline-block";
-          document.getElementById("mySidebar").style.display = "block";
-
-        }
-    
+// When the user clicks on div, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
