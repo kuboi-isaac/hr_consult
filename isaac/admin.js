551c999fe7c136e3cce4cb0dc6d13a58cc90b4a5
kuboi-isaac <123374858+kuboi-isaac@users.noreply.github.com>
2023-08-25 15:38:30 +0300

    25/8/2023 changes

--- isaac/admin.js
+++ isaac/admin.js
@@ -4,3 +4,21 @@ function myFunction() {
   popup.classList.toggle("show");
 }
 
+// You might need this if you have multiple popups on your page
+// const notificationPopup = document.getElementById("notificationPopup");
+
+// You can directly use this if you have only one popup
+const notificationPopup = document.querySelector(".popup");
+
+document.getElementById("notification").addEventListener("click", function(event) {
+  event.preventDefault();
+  event.stopPropagation(); // Prevents click from propagating to document and immediately hiding the popup
+  notificationPopup.style.display = "block";
+});
+
+// Close the popup when clicking outside of it
+document.addEventListener("click", function(event) {
+  if (!notificationPopup.contains(event.target)) {
+    notificationPopup.style.display = "none";
+  }
+});
